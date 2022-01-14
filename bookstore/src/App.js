37d4fa34f86d0dea4f06094232d115e6b6fe8c12
staffanholmnas <staffan.holmnas@centria.fi>
2022-01-14 18:50:09 +0200

    Add delete functionality

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -5,6 +5,8 @@ import 'ag-grid-community/dist/styles/ag-theme-material.css';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
 import AddBook from './AddBook';
 import './App.css';
 
@@ -13,13 +15,22 @@ function App() {
 
   useEffect(() => {
     fetchItems();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
   const fetchItems = () => {
     fetch('https://bookstore-53b08-default-rtdb.europe-west1.firebasedatabase.app/books/.json')
     .then(response => response.json())
-    .then(data => setBooks(Object.values(data)))
+    .then(data => addKeys(data))
     .catch(err => console.error(err))
+  }
+
+   // Add keys to the book objects
+   const addKeys = (data) => {
+    const keys = Object.keys(data);
+    const valueKeys = Object.values(data).map((item, index) => 
+    Object.defineProperty(item, 'id', {value: keys[index]}));
+    setBooks(valueKeys);
   }
 
   const addBook = (newBook) => {
@@ -27,6 +38,15 @@ function App() {
     {
       method: 'POST',
       body: JSON.stringify(newBook)
+    })
+    .then(response => fetchItems())
+    .catch(err => console.error(err))
+  }
+
+  const deleteBook = (id) => {
+    fetch(`https://bookstore-53b08-default-rtdb.europe-west1.firebasedatabase.app/books/${id}.json`,
+   {
+      method: 'DELETE',
     })
     .then(response => fetchItems())
     .catch(err => console.error(err))
@@ -43,13 +63,23 @@ function App() {
       </AppBar> 
       <p></p>
       <AddBook addBook={addBook}/> 
-      <div className="ag-theme-material" style={ { height: 600, width: 1000, margin: 'auto' } }>
+      <div className="ag-theme-material" style={ { height: 600, width: 1200, margin: 'auto' } }>
         <AgGridReact rowData={books}>
           <AgGridColumn sortable={true} filter={true} field='author' />
           <AgGridColumn sortable={true} filter={true} field='isbn' />
           <AgGridColumn sortable={true} filter={true} field='price' />
           <AgGridColumn sortable={true} filter={true} field='title' />
           <AgGridColumn sortable={true} filter={true} field='year' />
+          <AgGridColumn 
+            headerName=''
+            field='id' 
+            width={90}
+            cellRendererFramework={ params => 
+              <IconButton onClick={() => deleteBook(params.value)} size="small" color="error">
+                <DeleteIcon />
+              </IconButton>
+            }
+          />      
         </AgGridReact>
       </div>
     </div>
