44791ac0af280acb355027dfc06d1f4ce398adad
staffanholmnas <staffan.holmnas@centria.fi>
2022-01-14 17:09:00 +0200

    add grid

--- bookstore/src/App.js
+++ bookstore/src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { AgGridColumn, AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-material.css';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 import './App.css';
 
 function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetchItems();
+  }, [])
+
+  const fetchItems = () => {
+    fetch('https://bookstore-53b08-default-rtdb.europe-west1.firebasedatabase.app/books/.json')
+    .then(response => response.json())
+    .then(data => setBooks(Object.values(data)))
+    .catch(err => console.error(err))
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h5" noWrap>
+            Bookstore
+          </Typography>
+        </Toolbar>
+      </AppBar> 
+      <p></p>
+      <div className="ag-theme-material" style={ { height: 600, width: 1000, margin: 'auto' } }>
+        <AgGridReact rowData={books}>
+          <AgGridColumn sortable={true} filter={true} field='author' />
+          <AgGridColumn sortable={true} filter={true} field='isbn' />
+          <AgGridColumn sortable={true} filter={true} field='price' />
+          <AgGridColumn sortable={true} filter={true} field='title' />
+          <AgGridColumn sortable={true} filter={true} field='year' />
+        </AgGridReact>
+      </div>
     </div>
   );
 }
